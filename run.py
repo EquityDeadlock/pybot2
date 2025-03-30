05b6e4b1938a402fde6f148b4848859fcc98fb01
Braydon Giallombardo <braydon.giallombardo@gmail.com>
2025-03-30 07:36:13 -0400

    Add messages table to schema
    Messages in guilds will be recorded into the table along with the if of
    the message they are replying to if it is a reply

--- run.py
+++ run.py
@@ -4,9 +4,10 @@ import signal
 import sys
 import os
 import sqlite3
+from enum import IntEnum
 from dotenv import dotenv_values
 
-TESTING_PRINT_TO_CONSOLE = False
+TESTING_PRINT_TO_CONSOLE = True
 
 def signal_handler(sig, frame):
     if TESTING_PRINT_TO_CONSOLE:
@@ -18,6 +19,12 @@ def signal_handler(sig, frame):
             print('failed to destroy database')
     sys.exit(0)
 
+class MessageAction(IntEnum):
+    CREATE = 1
+    EDIT = 2
+    REPLY = 3
+    DELETE = 4
+
 class PyClient(discord.Client):
     async def on_ready(self):
         try:
@@ -25,12 +32,12 @@ class PyClient(discord.Client):
             cursor = sqlConnection.cursor()
             for guild in self.guilds:
                 print(guild.name)
-                query = f'''INSERT OR IGNORE INTO guilds (id, name, shard_id, chunked, member_count) VALUES (?,?,?,?,?);'''
+                query = '''INSERT OR IGNORE INTO guilds (id, name, shard_id, chunked, member_count) VALUES (?,?,?,?,?);'''
                 params = (guild.id, guild.name, guild.shard_id, 1 if guild.chunked else 0, guild.member_count)
                 cursor.execute(query, params)
-            query = '''SELECT * FROM guilds;'''
-            cursor.execute(query)
             if TESTING_PRINT_TO_CONSOLE:
+                query = '''SELECT * FROM guilds;'''
+                cursor.execute(query)
                 result = cursor.fetchall()
                 print(result)
         except sqlite3.Error as error:
@@ -41,6 +48,27 @@ class PyClient(discord.Client):
         print(f'Channel List:')
 
     async def on_message(self, message: discord.Message):
+        print(f'Message from guild: [{message.guild.id},{message.guild.name}] - {message.author}: {message.content}')
+        if isinstance(message, discord.channel.DMChannel):
+            return
+        try:
+            sqlConnection = sqlite3.connect('sql.db')
+            cursor = sqlConnection.cursor()
+
+            query = '''INSERT OR IGNORE INTO messages (id, guild_id, channel_id, user_id, action, content, reply_id, time) VALUES (?,?,?,?,?,?,?,?);'''
+            params = (message.id, message.guild.id, message.channel.id, message.author.id, int(MessageAction.REPLY) if message.reference is not None else int(MessageAction.CREATE), message.content, message.to_reference().message_id if message.reference is not None else None, message.created_at)
+            cursor.execute(query, params)
+            if TESTING_PRINT_TO_CONSOLE:
+                query = '''SELECT * FROM messages;'''
+                cursor.execute(query)
+                result = cursor.fetchall()
+                print(result)
+        except sqlite3.Error as error:
+            print(f'Error: {error}')
+        finally:
+            sqlConnection.close()
+
+
         print(f'Message from guild: [{message.guild.id},{message.guild.name}] - {message.author}: {message.content}')
 
 def sql_init_schema():
@@ -60,6 +88,19 @@ def sql_init_schema():
                 id INTEGER NOT NULL PRIMARY KEY,
                 guild_id INTEGER NOT NULL,
                 name TEXT NOT NULL);'''
+        cursor.execute(query)
+
+        query = '''CREATE TABLE IF NOT EXISTS messages(
+                id INTEGER NOT NULL,
+                guild_id INTEGER NOT NULL,
+                channel_id INTEGER NOT NULL,
+                user_id INTEGER NOT NULL,
+                action INTEGER NOT NULL,
+                content BLOB NOT NULL,
+                reply_id INTEGER,
+                time BLOB NOT NULL,
+                PRIMARY KEY(id, guild_id, channel_id, user_id));
+            '''
         cursor.execute(query)
 
         query = '''CREATE TABLE IF NOT EXISTS levels(
